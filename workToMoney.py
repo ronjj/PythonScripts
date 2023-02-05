081c2d05d6485f8dcfd79d03b7e2a0653548808c
Ronald Jabouin <37760008+ronjj@users.noreply.github.com>
2023-02-05 12:38:36 -0500

    added yahoo finance API to project

--- workToMoney.py
+++ workToMoney.py
@@ -1,6 +1,10 @@
 
-# Script To Calculate The Amount Of Money I'll Make From Work
-# Time Spent: ~30 minutes
+# Script To Calculate The Amount Of Money I'll Make From Work 
+# Also added the ability to get stock prices if I want to invest my money
+
+# Time Spent: ~45 minutes
+
+import yfinance as yf
 
 def workToMoney(week1Hours, week2Hours):
     
@@ -23,3 +27,16 @@ if above100:
 
 
 print(f"Glooks, gonna make ${round(result, 2)}. Investing 30% would be {round(thirtyPercent, 2)}." if above100 else f"Get those numbers up, only making ${round(result, 2)}")
+
+if above100:
+    wantToInvest = str(input("Do you want to invest?"))
+
+    if wantToInvest == 'y':
+        whereToInvest = str(input("Which stock do you want to invest in?"))
+        stockInfo = yf.Ticker(whereToInvest)
+        # stockInfoToDisplay = json.dumps(stockInfo.info, sort_keys=Fåalse, indent=4)
+
+        print(f"The current price of {whereToInvest} is {stockInfo.info['ask']}")
+        print(f"You could buy {thirtyPercent / int(stockInfo.info['ask']) } shares of {whereToInvest}.")
+        #TODO: check for response from yfinance. If respone is invalid, let the user know they entered an invalid
+        #stock ticker 
